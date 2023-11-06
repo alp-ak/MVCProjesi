78940dea37a2738f6ba68596cb38077f595094ec
alp-ak <148430972+alp-ak@users.noreply.github.com>
2023-11-06 15:30:31 +0300

    BilgeShop son hali

---
+++ BilgeShop/BilgeShop.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BilgeShop.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("urunler/{cateogryName}/{categoryId}")]
+        public IActionResult Index(int categoryId)
+        {
+            ViewBag.CategoryId = categoryId;
+
+            return View();
+        }
+    }
+}
---
+++ BilgeShop/BilgeShop.WebUI/ViewComponents/ProductsViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using BilgeShop.Business.Services;
+using BilgeShop.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilgeShop.WebUI.ViewComponents
+{
+    public class ProductsViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public ProductsViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IViewComponentResult Invoke(int? categoryId = null)
+        {
+            var productDtos = _productService.GetProductsByCategoryId(categoryId);
+
+            var viewModel = productDtos.Select(x => new ProductViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                CategoryId = x.CategoryId,
+                CategoryName = x.CategoryName,
+                UnitPrice = x.UnitPrice,
+                UnitsInStock = x.UnitsInStock,
+                ImagePath = x.ImagePath
+            }).ToList();
+
+            return View(viewModel);
+        }
+
+
+
+
+    }
+}
